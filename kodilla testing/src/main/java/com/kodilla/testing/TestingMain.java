55fb5bf9a331ff1df2d4249b99c4b81f898690f8
kati3006 <katarzynaczerw@gmail.com>
2017-11-11 18:04:26 +0100

    refactor

--- kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,11 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calc.Calc;
 
 public class TestingMain {
     public static void main(String[] args) {
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
         Calc calc = new Calc();
-        System.out.println(calc.add(12, 3));
-        System.out.println(calc.sub(12, 5));
+
+        if(calc.add(12,3)==15){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Wystapil blad");
+        }
+
+        if(calc.sub(12,3)==9){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Wystapil blad");
+        }
     }
 }
