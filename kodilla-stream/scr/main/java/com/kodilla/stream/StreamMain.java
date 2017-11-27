4a5aaad8dc23647019e3559189c0f5433c134dad
kati3006 <katarzynaczerw@gmail.com>
2017-11-28 00:33:02 +0100

    refactor

--- kodilla-stream/scr/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/scr/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,18 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("text", t -> t + "ABC"));
-        System.out.println(poemBeautifier.beautify("text", t -> t.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("text", t -> "```" + t + "```"));
-        System.out.println(poemBeautifier.beautify("text", t -> t.replaceFirst("t", "T")));
+      PoemBeautifier poemBeautifier = new PoemBeautifier();
+      System.out.println(poemBeautifier.beautify("text", t -> t + "ABC"));
+      System.out.println(poemBeautifier.beautify("text", t -> t.toUpperCase()));
+      System.out.println(poemBeautifier.beautify("text", t -> "```" + t + "```"));
+      System.out.println(poemBeautifier.beautify("text", t -> t.replaceFirst("t", "T")));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
