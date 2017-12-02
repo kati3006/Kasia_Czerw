c45db021ad693f8a4a6a0cb3681ce5b1f2c9ddfb
kati3006 <katarzynaczerw@gmail.com>
2017-12-02 15:54:50 +0100

    refactor

--- kodilla-stream/scr/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/scr/main/java/com/kodilla/stream/StreamMain.java
@@ -2,7 +2,6 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,21 +9,12 @@ public class StreamMain {
     public static void main(String[] args) {
         Forum theForum = new Forum();
 
-       theForum.getUserList().stream()
+        Map<Integer, ForumUser> theResultMapOfUsers = theForum.getUserList().stream()
                 .filter(s -> s.getSex() == 'M')
-                .forEach(s -> System.out.println(s.getSex()));
-
-       theForum.getUserList().stream()
-               .filter(d -> d.getDateOfBirth().getYear() > 1997)
-               .forEach(d -> System.out.println(d.getDateOfBirth()));
-
-       theForum.getUserList().stream()
-               .filter(p -> p.getQuantityOfPosts() >= 1)
-               .forEach(p -> System.out.println(p.getQuantityOfPosts()));
-
-        Map<Integer, Forum> theResultMapOfUsers = theForum.getUserList().stream()
-                .filter(user -> user.getUserID())
+                .filter(d -> d.getDateOfBirth().getYear() > 1997)
+                .filter(p -> p.getQuantityOfPosts() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserID, user -> user));
+
 
         System.out.println("# elements: " + theResultMapOfUsers.size());
         theResultMapOfUsers.entrySet().stream()
@@ -32,4 +22,3 @@ public class StreamMain {
                 .forEach(System.out::println);
     }
 }
-
--- kodilla-stream/scr/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/scr/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream.forumuser;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,16 +8,16 @@ public class Forum {
     private final List<ForumUser> userList = new ArrayList<>();
 
     public Forum() {
-       userList.add(new ForumUser(1,"aaa",'F',1973,33));
-        userList.add(new ForumUser(2,"bbb",'M',1993,6));
-        userList.add(new ForumUser(3,"ccc",'M',1986,0));
-        userList.add(new ForumUser(4,"ddd",'F',1989,17));
-        userList.add(new ForumUser(5,"eee",'M',1998,4));
-        userList.add(new ForumUser(6,"fff",'F',2000,5));
-        userList.add(new ForumUser(7,"ggg",'F',2002,36));
-        userList.add(new ForumUser(8,"hhh",'M',1986,29));
-        userList.add(new ForumUser(9,"iii",'M',1987,7));
-        userList.add(new ForumUser(10,"jjj",'F',1988,16));
+       userList.add(new ForumUser(1,"aaa",'F', LocalDate.of(1984,1,1),33));
+        userList.add(new ForumUser(2,"bbb",'M', LocalDate.of(1993, 1,1),6));
+        userList.add(new ForumUser(3,"ccc",'M', LocalDate.of(1986, 1,1 ),0));
+        userList.add(new ForumUser(4,"ddd",'F', LocalDate.of(1989, 1,1),17));
+        userList.add(new ForumUser(5,"eee",'M', LocalDate.of(1998, 1,1 ),4));
+        userList.add(new ForumUser(6,"fff",'F', LocalDate.of(2000, 1,1),5));
+        userList.add(new ForumUser(7,"ggg",'F', LocalDate.of(2002, 1,1),36));
+        userList.add(new ForumUser(8,"hhh",'M', LocalDate.of(1986,1,1),29));
+        userList.add(new ForumUser(9,"iii",'M', LocalDate.of(1987, 1,1),7));
+        userList.add(new ForumUser(10,"jjj",'F', LocalDate.of(1988,1,1),16));
     }
 
     public List<ForumUser> getUserList() {
